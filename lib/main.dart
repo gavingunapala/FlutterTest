d0728cdc916049c7ec2f8f37536b516de96cbb43
gavin <gavingunapala06@gmail.com>
2021-08-02 15:18:44 +0530

    add colors and center text

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,27 @@ void main() =>runApp(MaterialApp(
     appBar: AppBar(
       title: Text("My First App"),
       centerTitle: true,
+      backgroundColor: Colors.orange[700],
     ),
 
 
     body: Center(
-      child: Text("Hello Everyone !!!!!!"),
+      child: Text(
+          "Hello Everyone !!!!!!",
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2,
+          color: Colors.greenAccent,
+        ),
+      ),
     ),
 
 
     floatingActionButton: FloatingActionButton(
       onPressed: () {  },
       child: Text("Add"),
+      backgroundColor: Colors.amberAccent[100],
     ),
 
 
