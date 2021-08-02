6e407d6a1cad02865c059b3c101859607808170d
gavin <gavingunapala06@gmail.com>
2021-08-02 15:50:50 +0530

    create stateless functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() =>runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("My First App"),
-      centerTitle: true,
-      backgroundColor: Colors.orange[700],
-    ),
+  home:Home(),
+));
 
 
-    body: Center(
-      child: Text(
+
+//stless and press tab
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+        centerTitle: true,
+        backgroundColor: Colors.orange[700],
+      ),
+
+
+      body: Center(
+        child: Text(
           "Hello Everyone !!!!!!",
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2,
-          color: Colors.greenAccent,
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+            color: Colors.greenAccent,
+          ),
         ),
       ),
-    ),
 
 
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      child: Text("Add"),
-      backgroundColor: Colors.amberAccent[100],
-    ),
 
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Text("Add"),
+        backgroundColor: Colors.amberAccent[100],
+      ),
+    );
+  }
+}
 
-  ),
-));
